469637b74aac471c5f63a645b5a8582824483460
janyk <janyk.theron@icloud.com>
2021-03-17 23:21:22 +1300

    chore: add type check
    *update handler types
    * ignore transpiled assets

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,16 +1,22 @@
-import 'source-map-support/register'
+import { APIGatewayProxyHandler, APIGatewayEvent, Context, Callback } from 'aws-lambda';
+import 'source-map-support/register';
 
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway'
-import { formatJSONResponse } from '@libs/apiGateway'
-import { middyfy } from '@libs/lambda'
-
-import schema from './schema'
-
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  })
-}
-
-export const main = middyfy(hello)
+export const hello: APIGatewayProxyHandler = async (
+  event: APIGatewayEvent,
+  context: Context,
+  callback: Callback,
+): Promise<any> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: event,
+        context: context,
+        callback: callback,
+      },
+      null,
+      2,
+    ),
+  };
+};
