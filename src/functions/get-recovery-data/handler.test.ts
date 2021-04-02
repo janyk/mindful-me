a61f02295af265c8374146745e14be59558832e2
janyk <janyk.theron@icloud.com>
2021-04-02 13:58:57 +1300

    chore: add husky and improve broken test

--- src/functions/get-recovery-data/handler.test.ts
+++ src/functions/get-recovery-data/handler.test.ts
@@ -1,8 +1,8 @@
 import { Context, Callback, ScheduledEvent } from 'aws-lambda'
 import RecoveryDataException from '@errors/RecoveryDataException'
-import { getRecoveryData, handler, RECOVERY_FETCH_EXCEPTION_MESSAGE } from '@functions/get-recovery-data/handler'
+import { handler, RECOVERY_FETCH_EXCEPTION_MESSAGE } from '@functions/get-recovery-data/handler'
 
-// kind of filthy, but non of these arguments properties are being used so no need to mock them
+// kind of filthy, but none of these arguments properties are being used so no need to mock them
 const mockContext = ({} as unknown) as Context
 const mockScheduledEvent = ({} as unknown) as ScheduledEvent
 
@@ -17,16 +17,17 @@ jest.mock('@libs/whoop', () => ({
   })),
 }))
 
-describe('getRecoveryData', () => {
+describe('Get recovery data lambda', () => {
   afterEach(() => {
     jest.clearAllMocks()
   })
   it('should return the value of getRecoveryData ', async () => {
     mockGetMostRecentCycle.mockReturnValue(42)
-    await getRecoveryData()
-
-    expect(mockGetMostRecentCycle).toHaveBeenCalledWith(42)
+    
+    await handler(mockScheduledEvent, mockContext, mockCallback)
+    
     expect(mockGetMostRecentCycle).toHaveBeenCalledTimes(1)
+    expect(mockCallback).toBeCalledWith(null, { recoveryData: 42 })
   })
 
   it('should return an error instance of RecoveryDataException if there is any error', async () => {
